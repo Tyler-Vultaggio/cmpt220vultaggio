db8bbbdd0ac74a04218ca15ed63e5c30edf12c5a
Tyler Vultaggio <tylervultaggio101@gmail.com>
2020-02-07 22:57:25 -0500

    Solved Problem 2

---
+++ labs/1/Problem2.java
@@ -0,0 +1,12 @@
+
+public class Problem2 
+{
+
+	public static void main(String[] args)
+	{
+		//This is the equation to the problem 2
+		System.out.print("This is the solution to problem 2:" + 4*(1-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)));
+
+	}
+
+}
