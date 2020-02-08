512921426399f1a03f535bfc79eae18bbbe12db0
Tyler Vultaggio <tylervultaggio101@gmail.com>
2020-02-07 22:57:11 -0500

    Solved Problem 1

---
+++ labs/1/Problem1.java
@@ -0,0 +1,12 @@
+
+public class Program1 
+{
+
+	public static void main(String[] args)
+	{
+		//This is the equation for problem 1
+		System.out.print("This is the solution to problem 1:" + ((15.4*6.5-4.3*3)/(46.3-3.5)));
+
+	}
+
+}
