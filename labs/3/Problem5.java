2ca09131c8216f290686d97bdf2f4ae71f39378a
Tyler Vultaggio <tylervultaggio101@gmail.com>
2020-02-22 00:55:46 -0500

    Solution to Problem5 sorry for the hour late.

---
+++ labs/3/Problem5.java
@@ -0,0 +1,19 @@
+//Tyler Vultaggio
+//Lab 3 
+//2/18/2020
+
+public class Problem5 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.print(average(1,2,3));
+
+	}
+	
+	public static double average(double a, double b, double c)
+	{
+		return((a*b*c)/3);
+	}
+
+}
