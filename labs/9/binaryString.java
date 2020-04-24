f50ad2a58a11fcfcd91b9f5271d377892b23c0fc
Tyler Vultaggio <tylervultaggio101@gmail.com>
2020-04-24 18:39:38 -0400

    Solution to problem 1.

---
+++ labs/9/binaryString.java
@@ -0,0 +1,30 @@
+//Tyler Vultaggio
+//4/24/20
+//Lab 9
+
+public class binaryString 
+{
+    public static void main(String[] args) 
+    {
+    	System.out.print(bin2Dec("1010001001001001010101010100101010"));
+    }
+    
+    public static int bin2Dec(String binaryString)
+    {
+        
+        double j=0;
+        for(int i=0; i < binaryString.length(); i++)
+        {
+            if(binaryString.charAt(i)== '1' || binaryString.charAt(i)== '0') 
+            {
+                throw new  NumberFormatException();
+            }
+            if(binaryString.charAt(i)== '1')
+            {
+                j = j+ Math.pow(2, binaryString.length()-1-i);
+            }
+
+        }
+        return (int) j;
+    }
+}
